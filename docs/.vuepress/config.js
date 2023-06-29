00ab8a73e71c35975eb99bda4548ed0abf738bf6
86152 <1362747420@qq.com>
2023-06-29 22:40:22 +0800

    修改文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,90 +1,104 @@
 module.exports = {
   title: "KanadeBot",
-  description: '一只可爱奏宝Bot的文档',
-  dest: 'public',
-  base: '/docs/',
+  description: "一只可爱奏宝Bot的文档",
+  dest: "public",
+  base: "/docs/",
   locales: {
-    '/': {
-      lang: 'zh-CN',
+    "/": {
+      lang: "zh-CN",
       recoLocales: {
         pagation: {
-          prev: '上一页',
-          next: '下一页',
-          go: '前往',
-          jump: '跳转至'
-        }
-      }
-    }
+          prev: "上一页",
+          next: "下一页",
+          go: "前往",
+          jump: "跳转至",
+        },
+      },
+    },
   },
   head: [
-    ['link', { rel: 'icon', href: '/image/icon.png' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ["link", { rel: "icon", href: "/image/icon.png" }],
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   plugins: [
-    ["sakura", {
-      num: 10,  // 默认数量
-      show: true, //  是否显示
-      zIndex: -1,   // 层级
-      img: {
-        replace: true,  // false 默认图 true 换图 需要填写httpUrl地址
-        httpUrl: '/docs/image/cup.png'     // 绝对路径
-      }     
-    }]
+    [
+      "sakura",
+      {
+        num: 10, // 默认数量
+        show: true, //  是否显示
+        zIndex: -1, // 层级
+        img: {
+          replace: true, // false 默认图 true 换图 需要填写httpUrl地址
+          httpUrl: "/docs/image/cup.png", // 绝对路径
+        },
+      },
+    ],
   ],
-  theme: 'reco',
+  theme: "reco",
   themeConfig: {
-    logo: '/image/head.png',
+    logo: "/image/head.png",
     nav: [
-      { text: '主页', link: '/', icon: 'reco-home' },
-      { text: '使用文档', link: '/use/', icon: 'reco-document' },
-      { text: '关于Bot', link: '/about.md', icon: 'reco-faq' },
-      { text: '更新日志', link: '/log/', icon: 'reco-date' },
+      { text: "主页", link: "/", icon: "reco-home" },
+      { text: "使用文档", link: "/use/", icon: "reco-document" },
+      { text: "关于Bot", link: "/about.md", icon: "reco-faq" },
+      { text: "更新日志", link: "/log/", icon: "reco-date" },
     ],
     search: true,
     searchMaxSuggestions: 10,
     sidebar: {
-      '/use/': [
+      "/use/": [
         {
-          title: '前言',
-          path: '/use/'
+          title: "前言",
+          path: "/use/",
         },
         {
-          title: '通用插件',
+          title: "通用插件",
           sidebarDepth: 1,
           children: [
-          'common/pjsk',
-          'common/relax', 
-          'common/shop', 
-          'common/tools',
-          'common/info', 
-          'common/genepic',
-          'common/image', 
-          'common/other',
-          'common/webgame'
-          ]
+            "common/pjsk",
+            "common/relax",
+            "common/shop",
+            "common/tools",
+            "common/info",
+            "common/genepic",
+            "common/image",
+            "common/other",
+            "common/webgame",
+          ],
         },
         {
-          title: '群管插件',
-          path: 'admin/'
+          title: "群管插件",
+          path: "admin/",
         },
-        
+
         {
-          title: '被动插件',
-          path: 'passive/'
+          title: "被动插件",
+          path: "passive/",
         },
-      ]
+      ],
     },
-    lastUpdated: '最近更新于：',
-    author: 'Yozora',
-    subSidebar: 'auto',
-    repo: 'cYanosora/kndbot',
-    docsRepo: 'cYanosora/kndbot',
-    docsDir: 'docs',
-    docsBranch: 'main',
+    lastUpdated: "最近更新于：",
+    author: "Yozora",
+    subSidebar: "auto",
+    repo: "cYanosora/kndbot",
+    docsRepo: "cYanosora/kndbot",
+    docsDir: "docs",
+    docsBranch: "main",
     editLinks: true,
-    editLinkText: '对此有疑问?帮助改进文档'
-  }
-}  
+    editLinkText: "对此有疑问?帮助改进文档",
+    // 备案
+    record: "湘ICP备2023008035号-1",
+    recordLink: "https://beian.miit.gov.cn/",
+    // 项目开始时间，只填写年份
+    startYear: "2023",
+  },
+};  
